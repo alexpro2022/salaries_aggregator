5bbb0a8f8ee056180c9d93da88e3c41a28b60cc4
Alex Proskuriakov <alexpro1972@yandex.ru>
2024-02-29 11:44:53 +0300

    Added testing

---
+++ test/test.py
@@ -0,0 +1,20 @@
+import logging
+from src.calculation import calc
+from test.test_data import test_data
+
+logging.basicConfig(level=logging.INFO)
+
+
+async def test(collection):
+    for input_data, expected_result in test_data:
+        actual_result = await calc(collection, input_data)
+        try:
+            assert actual_result == expected_result
+            logging.info('=== SUCCESS !!! ===')
+        except AssertionError:
+            logging.info('============START============')
+            logging.info(expected_result)
+            logging.info('--------------------------')
+            logging.info(actual_result)
+            logging.info('============END============')
+            assert 0
